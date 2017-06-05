5a657ad965536002f46cb2f93ce2d816562d99a1
jhancock <jessehancock1@gmail.com>
2017-06-05 12:07:35 -0600

    debug trailing comma in package.json

---
+++ webpack104/webpack.config.js
@@ -0,0 +1,32 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'app.bundle.js'
+    },
+    module: {
+      rules: [
+        {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: ['css-loader', 'sass-loader']
+      })}
+      ]
+    },
+   plugins: [
+     new HtmlWebpackPlugin({
+      title: 'Project Demo',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+    }),
+    new ExtractTextPlugin('app.css')
+]
+}﻿
