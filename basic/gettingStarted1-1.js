ddf0e9d815445bf223e0a6832f9694960f206289
jhancock <jhancock@jhancock.local>
2017-06-05 08:53:23 -0600

    basic webpack setup

---
+++ basic/gettingStarted1-1.js
@@ -0,0 +1,22 @@
+//Use this at https://jscomplete.com/repl
+
+class Button extends React.Component {
+	state = {counter:0};
+
+  handleClick = () => {
+   this.setState((prevState) => {
+   return {
+   	   counter:prevState.counter + 1
+   }})
+  }
+
+	render() {
+		return (
+			<button onClick={this.handleClick}>
+      {this.state.counter}
+      </button>
+		);
+	};
+}
+
+ReactDOM.render(<Button label="Do"/>, mountNode)
---
+++ webpack102/webpack.config.js
@@ -0,0 +1,18 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'app.bundle.js'
+    },
+   plugins: [new HtmlWebpackPlugin({
+    title: 'Project Demo',
+    minify: {
+      collapseWhitespace: true
+    },
+    hash: true,
+    template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+  })]
+}﻿
